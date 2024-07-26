defd370f315d6f5fb17fbea8bf8a62fb83c7faf5
xenoxvax <s.rhanmia@gmail.com>
2024-07-26 13:43:47 +0200

    Update index.ts

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -2,7 +2,12 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'dark',
-  fontFamily: 'Roboto',
+  colors: {
+    dark: [
+      '#01010a',
+    ],
+  },
+  fontFamily: 'Montserrat',
   shadows: { sm: '1px 1px 3px rgba(0, 0, 0, 0.5)' },
   components: {
     Button: {
